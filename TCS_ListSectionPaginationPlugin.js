cb16e83173a4808d93a342698d8d5c2fc598e39f
Alistair Everington <hello@thecodeshop.co>
2025-02-11 15:55:19 +0000

    Add files via upload

--- TCS_ListSectionPaginationPlugin.js
+++ TCS_ListSectionPaginationPlugin.js
@@ -1,8 +1,14 @@
+// ---- MAIN PLUGIN FUNCTION ---- //
+
+// Helper function to detect targeting code block and find list section //
+
 function findListSection() {
    let paginationMarker = document.querySelector('#paginated-list-section');
    let paginationTarget = paginationMarker.closest('section').nextElementSibling;
    return { paginationTarget, paginationMarker };
 }
+
+// Helper function to create a new 'page' full of items //
 
 function createNewListPage(paginationTarget) {
     let originalList = paginationTarget.querySelector('.user-items-list-simple');
@@ -14,31 +20,7 @@ function createNewListPage(paginationTarget) {
     return newListPage;
 };
 
-function createPagination() {
-    let { paginationTarget, paginationMarker } = findListSection();
-    let originalList = paginationTarget.querySelector('.user-items-list-simple');
-    let pageItemLimit = Number(paginationMarker.attributes['data-items-per-page'].value);
-    let items = Array.from(originalList.children);
-    
-    let remainingItems = items.slice(pageItemLimit);
-    
-    while (remainingItems.length > pageItemLimit) {
-        let newListPage = createNewListPage(paginationTarget);
-        let overflowItems = remainingItems.splice(0, pageItemLimit);
-        newListPage.append(...overflowItems);
-    }
-    
-    if (remainingItems.length > 0) {
-        let newListPage = createNewListPage(paginationTarget);
-        newListPage.append(...remainingItems);
-    }
-
-    addPaginationButton(paginationTarget, paginationMarker);
-
-    paginationTarget.querySelector('.user-items-list').classList.add('list-wrapper_paginated');
-    paginationTarget.querySelector('.user-items-list').style.gap = window.getComputedStyle(paginationTarget.querySelector('.user-items-list-simple')).getPropertyValue('grid-gap');
-    paginationTarget.querySelector('.user-items-list-simple').classList.add('list_paginated');
-}
+// Helper function that checks if the user has enabled the additional button for the section and moves it to the bottom of the lists //
 
 function checkForSectionButton(paginationTarget) {
     let sectionButton = paginationTarget.querySelector('.list-section-button-container');
@@ -47,7 +29,12 @@ function checkForSectionButton(paginationTarget) {
     }
 }
 
+// Helper function that creates the pagination button, along with its event listener, and adds it to the bottom of the lists //
+
 function addPaginationButton(paginationTarget, paginationMarker) {
+    
+    // Creating the button and applying the correct styles and text //
+
     let buttonWrapper = document.createElement('div');
     buttonWrapper.id = 'list_pagination-button-wrapper';
     if (paginationTarget.querySelector('.user-items-list').getAttribute('data-layout-width') === 'inset') {
@@ -74,18 +61,80 @@ function addPaginationButton(paginationTarget, paginationMarker) {
     } 
     let buttonText = paginationMarker.getAttribute('data-pagination-button-text') || 'View More';
     button.textContent = buttonText;
+
+    // Creating and adding the event listner to the button //
+
     button.addEventListener('click', (event) => {
+        // Prevents the button from clicking through //
         event.preventDefault();
+        // Finding the list of pages that are hidden //
         let hiddenPages = paginationTarget.querySelectorAll('.user-items-list-simple[style*="display: none"]');
+        // If there are hidden pages, display the first one //
         if (hiddenPages.length > 0) {
             hiddenPages[0].style.display = 'grid';
         }
+        // If there are no hidden pages, hide the button //
         if (hiddenPages.length <= 1) {
             buttonWrapper.style.display = 'none';
         }
     });
+
+    // Appending the button to the end of the list //
+
     paginationTarget.querySelector('.user-items-list').appendChild(buttonWrapper);
+
+    // Calling the helper function to sort out the additional button if its there //
+
     checkForSectionButton(paginationTarget);
 }
+
+// Main function that creates the pagination for the list section //
+
+function createPagination() {
+
+    // Finds the list section and the code block target //
+
+    let { paginationTarget, paginationMarker } = findListSection();
+
+    // Finds the whole list of items //
+
+    let originalList = paginationTarget.querySelector('.user-items-list-simple');
+    let items = Array.from(originalList.children);
+
+    // Finds the amount of items per page and gets a list of remaining items left after the first page has been filled //
+
+    let pageItemLimit = Number(paginationMarker.attributes['data-items-per-page'].value);
+    let remainingItems = items.slice(pageItemLimit);
+    
+    // While there are more items remaining than the limit per page, create a new page and append the items to it //
+    
+    while (remainingItems.length > pageItemLimit) {
+        // Creates a new page for items to fill //
+        let newListPage = createNewListPage(paginationTarget);
+        // Defines a list of the first 'x' items to fill the new page //
+        let overflowItems = remainingItems.splice(0, pageItemLimit);
+        // Appends the items to the new page //
+        newListPage.append(...overflowItems);
+    }
+    
+    // If there are still items left after the while loop, create a new page and append the remaining items to it //
+    
+    if (remainingItems.length > 0) {
+        let newListPage = createNewListPage(paginationTarget);
+        newListPage.append(...remainingItems);
+    }
+
+    // Adds the pagination button to the bottom of the lists //
+
+    addPaginationButton(paginationTarget, paginationMarker);
+
+    // Applies CSS styles to the relevant items //
+
+    paginationTarget.querySelector('.user-items-list').classList.add('list-wrapper_paginated');
+    paginationTarget.querySelector('.user-items-list').style.gap = window.getComputedStyle(paginationTarget.querySelector('.user-items-list-simple')).getPropertyValue('grid-gap');
+    paginationTarget.querySelector('.user-items-list-simple').classList.add('list_paginated');
+}
+
+// Calling the function at the end of page load //
 
 document.addEventListener('DOMContentLoaded', createPagination);
